50e8b2e8ec5d5235d20e1f5bacab4cdad5a25c9c
tanitta <ttata.trit@gmail.com>
2015-06-07 23:08:24 +0900

    enable to work

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 #Minimal Slack
+chrome extension for slack.
 
 ##install
+[ref](https://google.com)
 
 ##Usage
+toggle sidebar : Ctrl + Y
--- myscript.js
+++ myscript.js
@@ -1,14 +1,36 @@
 document.OnLoad = start();
 function start() {
 	$("body").css({minWidth: 100});
-    window.alert('Hello World !!');
+	hide();
+	$("body").keydown(function(e){
+		if(event.ctrlKey){
+			if(e.keyCode === 89){
+				toggle_sidebar();
+				return false;
+			}
+		}
+	});
+}
+
+function toggle_sidebar(){
+	if ($("#col_channels").is(':visible')){
+		hide();
+	}else{
+		show();
+	}
 }
 
 function hide(){
+	$("#col_channels,#col_channels_bg,#team_menu").hide();
 	$("#footer").css({left:0});
+	$("#messages_container").css({marginLeft:0});
+	$(window).trigger('resize');
 }
 
 function show(){
+	$("#col_channels,#col_channels_bg,#team_menu").show();
+	$("#messages_container").css({marginLeft:220});
+	$("#footer").css({left:220});
+	$(window).trigger('resize');
 }
-
 
