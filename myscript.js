ecb8df6e2de14f3128a0528f59365421bce5746b
tanitta <ttata.trit@gmail.com>
2015-06-07 20:43:38 +0900

    add jquery

--- myscript.js
+++ myscript.js
@@ -1,5 +1,14 @@
 document.OnLoad = start();
 function start() {
 	$("body").css({minWidth: 100});
+    window.alert('Hello World !!');
 }
+
+function hide(){
+	$("#footer").css({left:0});
+}
+
+function show(){
+}
+
 
